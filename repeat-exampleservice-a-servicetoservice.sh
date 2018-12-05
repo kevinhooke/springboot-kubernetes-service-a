8848521b15bc28faac9290cd3e8df9aac86093e5
kevinhooke <kevin.hooke@gmail.com>
2018-12-04 23:04:27 -0800

    update to initial state

---
+++ repeat-exampleservice-a-servicetoservice.sh
@@ -0,0 +1,1 @@
+while true; do curl 35.247.9.131:8080/example-a/v1/servicetoservice; echo; sleep 2; done;
---
+++ repeat-local-exampleservice-a-servicetoservice.sh
@@ -0,0 +1,1 @@
+while true; do curl localhost:8080/example-a/v1/servicetoservice; echo; sleep 2; done;
